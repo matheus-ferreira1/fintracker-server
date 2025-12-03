172bfc05928b4b8c252017a7ddb90f5d10aea4b9
matheus-ferreira1 <matheus.fer33@gmail.com>
2025-12-03 15:42:51 -0300

    feat(type): added a index type with an apperror to unify error structure and pagination types

---
+++ src/types/index.ts
@@ -0,0 +1,26 @@
+export class AppError extends Error {
+  constructor(
+    public statusCode: number,
+    public message: string,
+    public isOperational = true
+  ) {
+    super(message);
+    Object.setPrototypeOf(this, AppError.prototype);
+  }
+}
+
+export interface PaginationParams {
+  page: number;
+  limit: number;
+  offset: number;
+}
+
+export interface PaginatedResponse<T> {
+  data: T[];
+  pagination: {
+    page: number;
+    limit: number;
+    total: number;
+    totalPages: number;
+  };
+}
