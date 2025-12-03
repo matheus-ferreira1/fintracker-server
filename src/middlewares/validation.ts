47f6d0d5d78f6a70ad7a9f830127356315df47e5
matheus-ferreira1 <matheus.fer33@gmail.com>
2025-12-03 17:44:14 -0300

    feat(middlewares): added a validation middleware to validate the request body

---
+++ src/middlewares/validation.ts
@@ -0,0 +1,21 @@
+import { AppError } from "@/types";
+import type { NextFunction } from "express";
+import type Joi from "joi";
+
+export const validate = (schema: Joi.ObjectSchema) => {
+  return (req: Request, _res: Response, next: NextFunction) => {
+    const { error } = schema.validate(req.body, {
+      abortEarly: false,
+      stripUnknown: true,
+    });
+
+    if (error) {
+      const errorMessage = error.details
+        .map((detail) => detail.message)
+        .join(", ");
+      throw new AppError(400, errorMessage);
+    }
+
+    next();
+  };
+};
